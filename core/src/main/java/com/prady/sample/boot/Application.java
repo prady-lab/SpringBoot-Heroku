76db0142b6c7bdd14f2c68d07e40867b1c673d4f
Pradeep <pradeep.b@tavant.com>
2016-03-23 20:42:49 +0530

    Added Basecode.

---
+++ core/src/main/java/com/prady/sample/boot/Application.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.prady.sample.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Prady
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ core/src/main/java/com/prady/sample/boot/HelloController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.prady.sample.boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Prady
+ *
+ */
+@RestController
+public class HelloController {
+	private static final String template = "Hello, %s!";
+
+	@RequestMapping("/hello")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format(template, name);
+	}
+}
